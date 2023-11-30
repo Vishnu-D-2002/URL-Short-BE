3a39196c5831d6c64b4dfe6c273f020af2cad6be
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-11-30 16:17:22 +0530

    all success

--- controllers/url.js
+++ controllers/url.js
@@ -1,11 +1,16 @@
 const URL_Short = require("../models/url");
+const User = require("../models/user");
 
 const urlController = {
     urlLong: async (req, res) => {
         try {
-            const longURL = req.body.longURL; 
+            const {longURL} = req.body; 
 
-            let url = await URL_Short.findOne({ longURL });
+            const userId = req.userId;
+            const user = await User.findById(userId);
+            if(user){
+            
+                let url = await URL_Short.findOne({ longURL });
 
             if (!url) {
                 url= new URL_Short({ longURL });
@@ -13,7 +18,10 @@ const urlController = {
                 res.status(200).json({ message: "New long URL saved" });
             } else {
                 res.status(200).json({ message: "URL already exists" });
+                }
             }
+            return res.status(500).json({ message: "User not Found" });
+            
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Internal Server Error", error });
