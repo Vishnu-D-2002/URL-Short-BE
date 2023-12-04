6fe513336bc1d156303ad597d7287e478f422ddc
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-04 16:55:29 +0530

    small changes

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,12 @@ userRoute.post('/signin', userController.signin);
 userRoute.post('/reset-password', userController.resetPassword);
 userRoute.post('/new-password', userController.newPassword);
 userRoute.get('/allusers', userController.allusers);
+userRoute.post('/url', authenticate, urlController.urlLong);
 userRoute.post('/:email', userController.activationLink);
 userRoute.get('/activate/:activationToken', userController.activateAccount);
 
 userRoute.get('/AllURL', authenticate, urlController.getURLs);
 userRoute.get('/', authenticate ,userController.getProfile);
-userRoute.post('/url', authenticate, urlController.urlLong);
 userRoute.get('/:shortString',urlController.getRedirect);
 userRoute.delete('/:shortString', authenticate, urlController.deleteURL);
 
